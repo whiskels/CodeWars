f86e1b2074e20b01b3a19929c7c5c5181f15703f
whiskels <kuzmingeo@gmail.com>
2020-06-18 17:37:53 +0300

    Added Snail Sort, Sudoku Solution Validator

---
+++ src/main/java/kyu4/SnailSort.java
@@ -0,0 +1,98 @@
+package main.java.kyu4;
+
+/**
+ * 4 kyu - Snail Sort
+ *
+ * https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/
+ *
+ * Details:
+ *
+ * Given an n x n array, return the array elements arranged from outermost elements to the middle
+ * element, traveling clockwise.
+ */
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import static java.util.stream.Collectors.joining;
+
+public class SnailSort {
+    public static int[] snail(int[][] array) {
+        if (array == null || array.length == 0 || array.length == 1 && array[0].length == 0) {
+            return new int[0];
+        }
+
+        final int height = array.length;
+        final int width = array[0].length;
+        final int lastY = height / 2;
+        final int lastX = width % 2 == 0 ? width / 2 - 1 : width / 2;
+
+
+        int y = 0, x = 0, dirX = 1, dirY = 0, i = 0;
+        int leftBorder = -1, rightBorder = width;
+        int upperBorder = -1, lowerBorder = height;
+        int[] output = new int[height * width];
+
+        while (true) {
+            if (x == rightBorder && y == upperBorder + 1) {
+                x--;
+                y++;
+                dirX = 0;
+                dirY = 1;
+                upperBorder++;
+            } else if (x == leftBorder && y == lowerBorder - 1) {
+                x++;
+                y--;
+                dirX = 0;
+                dirY = -1;
+                lowerBorder--;
+            } else if (y == lowerBorder && x == rightBorder - 1) {
+                y--;
+                x--;
+                dirX = -1;
+                dirY = 0;
+                rightBorder--;
+            } else if (y == upperBorder && x == leftBorder + 1) {
+                y++;
+                x++;
+                dirX = 1;
+                dirY = 0;
+                leftBorder++;
+            }
+            output[i] = array[y][x];
+
+            if (y == lastY && x == lastX) {
+                break;
+            }
+
+            x += dirX;
+            y += dirY;
+            i++;
+        }
+
+        return output;
+    }
+
+    @Test
+    public void SnailTest1() {
+        int[][] array
+                = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int[] r = {1, 2, 3, 6, 9, 8, 7, 4, 5};
+        test(array, r);
+    }
+
+    public String int2dToString(int[][] a) {
+        return Arrays.stream(a).map(row -> Arrays.toString(row)).collect(joining("\n"));
+    }
+
+    public void test(int[][] array, int[] result) {
+        String text = int2dToString(array) + " should be sorted to " + Arrays.toString(result);
+        System.out.println(text);
+        Assert.assertArrayEquals( result, snail(array));
+    }
+
+}
---
+++ src/main/java/kyu4/SudokuSolutionValidator.java
@@ -0,0 +1,140 @@
+package main.java.kyu4;
+
+/**
+ * 4 kyu - Sudoku Solution Validator
+ *
+ * https://www.codewars.com/kata/529bf0e9bdf7657179000008/
+ *
+ * Details:
+ *
+ * Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid
+ * with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids
+ * (also known as blocks) contain all of the digits from 1 to 9.
+ * (More info at: http://en.wikipedia.org/wiki/Sudoku)
+ *
+ * Sudoku Solution Validator
+ *
+ * Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array
+ * representing a Sudoku board, and returns true if it is a valid solution, or false otherwise.
+ * The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards
+ * containing one or more zeroes are considered to be invalid solutions.
+ *
+ * The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SudokuSolutionValidator {
+    private static int size;
+    private static ArrayList<Integer> values;
+
+    public static boolean check(int[][] sudoku) {
+        size = sudoku.length;
+        values = new ArrayList<>(size);
+        return check(sudoku, true) && check(sudoku, false) && blocksCheck(sudoku);
+    }
+
+    private final static boolean blocksCheck(int[][] sudoku) {
+        for (int y = 0; y < size; y += 3) {
+            for (int x = 0; x < size; x += 3) {
+                for (int j = y; j < y + 3; j++) {
+                    for (int i = x; i < x + 3; i++) {
+                        values.add(sudoku[j][i]);
+                    }
+                }
+                System.out.println(values.size());
+
+                if (!validateSection()) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private final static boolean validateSection() {
+        if (values.size() != size) {
+            return false;
+        }
+
+        Collections.sort(values);
+
+        if (values.get(0) != 1) {
+            return false;
+        }
+
+        for (int i = 1; i < size; i++) {
+            if (values.get(i) - values.get(i-1) != 1) {
+                return false;
+            }
+        }
+
+        values.clear();
+
+        return true;
+    }
+
+    private final static boolean check(int[][] sudoku, boolean isFlipped) {
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                int i, j;
+
+                if (isFlipped) {
+                    i = x;
+                    j = y;
+                } else {
+                    i = y;
+                    j = x;
+                }
+
+                if (sudoku[i][j] == 0) {
+                    return false;
+                }
+                values.add(sudoku[i][j]);
+            }
+
+            if (!validateSection()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Test
+    public void exampleTest() {
+        int[][] sudoku = {
+                {9, 8, 7, 2, 3, 1, 5, 4, 6},
+                {6, 5, 4, 8, 9, 7, 2, 1, 3},
+                {3, 2, 1, 5, 6, 4, 8, 7, 9},
+                {7, 6, 5, 9, 1, 8, 3, 2, 4},
+                {4, 3, 2, 6, 7, 5, 9, 8, 1},
+                {1, 9, 8, 3, 4, 2, 6, 5, 7},
+                {8, 7, 6, 1, 2, 9, 4, 3, 5},
+                {5, 4, 3, 7, 8, 6, 1, 9, 2},
+                {2, 1, 9, 4, 5, 3, 7, 6, 8}
+        };
+        assertEquals(true, check(sudoku));
+
+        sudoku[0][0]++;
+        sudoku[1][1]++;
+        sudoku[0][1]--;
+        sudoku[1][0]--;
+
+        assertEquals(false, check(sudoku));
+
+        sudoku[0][0]--;
+        sudoku[1][1]--;
+        sudoku[0][1]++;
+        sudoku[1][0]++;
+
+        sudoku[4][4] = 0;
+
+        assertEquals(false, check(sudoku));
+    }
+}
